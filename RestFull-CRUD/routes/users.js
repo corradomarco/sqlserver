365e4fccdcda4e81435d4cc467fc61c722d69e5f
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-01-08 10:27:45 +0000

    2

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql');
+const config = {
+  user: '4DD_10',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_10', //(Nome del DB)
+}
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/esami', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from esami', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
 });
 
 module.exports = router;
