4190498a32cbe4207699a0cfcfedddaff81a60d8
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-02-10 10:12:53 +0000

    aqwaqwaqwaqwa

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/unit', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select Unit from [cr-unit-attributes]', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.render(immagini);  //Invio il risultato
+    });
+    
+  });
+});
+
+
 module.exports = router;
