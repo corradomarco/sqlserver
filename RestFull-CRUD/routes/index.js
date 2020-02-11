e6d91ee93de99d701a3859b25731a393e72d36e7
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-02-11 11:45:55 +0000

    swswsw

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+const sql = require('mssql');
+var createError = require('http-errors');
+const config = {
+  user: '4DD_10',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_10', //(Nome del DB)
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/unit', function(req, res, next) {
+router.get('/immagini', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
@@ -14,7 +21,7 @@ router.get('/unit', function(req, res, next) {
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
     sqlRequest.query('select Unit from [cr-unit-attributes]', (err, result) => {
         if (err) console.log(err); // ... error checks
-        res.render(immagini);  //Invio il risultato
+        res.render('immagini',{dati:result.recordset});  //Invio il risultato
     });
     
   });
