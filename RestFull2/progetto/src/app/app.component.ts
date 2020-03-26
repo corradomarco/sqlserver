354a3933098f6ae1b5e89c064b010524d4a75500
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-03-26 16:28:23 +0000

    aaaa

--- RestFull2/progetto/src/app/app.component.ts
+++ RestFull2/progetto/src/app/app.component.ts
@@ -16,19 +16,29 @@ export class AppComponent {
   constructor(private http: HttpClient) { } //Dependency injection
   getUnitList(): void {
     //Qui va sostituito l’url con quello delle vostre api
-    this.obsUnit = this.http.get<Unit[]>('http://localhost:3000/users');
+    this.obsUnit = this.http.get<Unit[]>('https://3000-db1efc4d-8341-4f10-97b1-772b75753341.ws-eu01.gitpod.io/users');
     //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
     this.obsUnit.subscribe((data: Unit[]) => {this.data = data;});
   }
   postObserver : Observable<Object>;
 postData : Object;
-addUnit(newUnit: HTMLInputElement, newCost: HTMLInputElement, newHitSpeed: HTMLInputElement): boolean {
+addUnit(newUnit: HTMLInputElement,newRange:HTMLInputElement,newType:HTMLInputElement,newRarity:HTMLInputElement,newTarget:HTMLInputElement,newCount:HTMLInputElement,newTransport:HTMLInputElement, newCost: HTMLInputElement, newHitSpeed: HTMLInputElement,newSpeed: HTMLInputElement,newDeploytime: HTMLInputElement): boolean {
     let newData: Unit = new Unit();
     newData.Unit = newUnit.value;
     newData.Cost = newCost.value;
     newData.Hit_Speed = newHitSpeed.value;
+    newData.Speed= newSpeed.value;
+    newData.DeployTime=newDeploytime.value;
+    newData.Range =newRange.value;
+    newData.Target= newTarget.value;
+    newData.Count=newCount.value;
+    newData.Transport=newTransport.value;
+    newData.Type=newType.value;
+    newData.Rarity=newRarity.value;
+
+
     let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
-    this.postObserver = this.http.post('https://3000-db1efc4d-8341-4f10-97b1-772b75753341.ws-eu01.gitpod.io//users', JSON.stringify(newData),headers)
+    this.postObserver = this.http.post('https://3000-db1efc4d-8341-4f10-97b1-772b75753341.ws-eu01.gitpod.io/users', JSON.stringify(newData),headers)
     this.postObserver.subscribe(data => this.postData = data);
     return false;
   }
